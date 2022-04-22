e303109c4c2444eef9a4acc7bb7f8809c806ed8b
Andre Fristo <afrist@pop-os.localdomain>
2022-04-21 20:22:56 -0500

    Cleans up code and repo.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # systems-dynamics-research
+
+### Setup
+1. Install python
+2. Download the corpus (as text files) into the "input_data" folder.
+3. Install the relevant packages.
+    - ```pip install --upgrade gensim``` or ```conda install -c conda-forge gensim```
+4. Run the script: ```python topic_model.py```
--- topic_model.py
+++ topic_model.py
@@ -1,85 +1,130 @@
+#!/usr/bin/python3
 import pprint
 from gensim import models
 import json
+import glob
 import os
-
-"""text_corpus = [
-    "Human machine interface for lab abc computer applications",
-    "A survey of user opinion of computer system response time",
-    "The EPS user interface management system",
-    "System and human system engineering testing of EPS",
-    "Relation of user perceived response time to error measurement",
-    "The generation of random binary unordered trees",
-    "The intersection graph of paths in trees",
-    "Graph minors IV Widths of trees and well quasi ordering",
-    "Graph minors A survey",
-]"""
-
-
-# Folder Path
-path = "/home/atla/Documents/Programming/LDA practice/Documents"
-  
-# Change the directory
-os.chdir(path)
-  
-# Read text File
-  
-def read_text_file(file_path):
-    with open(file_path, 'r') as f:
-        val = f.read()
-    return val
-  
-
-text_corpus = []  
-print(len(text_corpus))
-
-# iterate through all file
-for file in os.listdir():
-    # Check whether file is in text format or not
-    if file.endswith(".txt"):
-        file_path = f"{path}/{file}"
-  
-        # call read text file function
-        result = read_text_file(file_path)
-        text_corpus.append(result)
-
-print(len(text_corpus))
-
-
-# Create a set of frequent words
-stoplist = set('for a of the and to in is be are as on by with or will that this at an from it not'.split(' '))
-# Lowercase each document, split it by white space and filter out stopwords
-texts = [[word for word in document.lower().split() if word not in stoplist]
-         for document in text_corpus]
-
-
-# Count word frequencies
 from collections import defaultdict
-frequency = defaultdict(int)
-for text in texts:
-    for token in text:
-        frequency[token] += 1
-
-# Only keep words that appear more than once
-processed_corpus = [[token for token in text if frequency[token] > 1] for text in texts]
-pprint.pprint(processed_corpus)
-
 from gensim import corpora
 
-dictionary = corpora.Dictionary(processed_corpus)
-#print(dictionary)
 
-pprint.pprint(dictionary.token2id)
+def train_model(text_corpus):
+    '''
+    A function for training the actual LDA model.
+    '''
 
-new_doc = "Human computer interaction"
-new_vec = dictionary.doc2bow(new_doc.lower().split())
-#print(new_vec)
+    # Create a set of frequent words we want to remove.
+    stoplist = set('for a of the and to in is be are as on by with or will that this at an from it not'.split(' '))
 
-bow_corpus = [dictionary.doc2bow(text) for text in processed_corpus]
-#pprint.pprint(bow_corpus)
+    # Conver each document to lowercase, split it by white space and filter out stopwords using list comprehension.
+    texts = [[word for word in document.lower().split() if word not in stoplist] for document in text_corpus]
 
-model = models.LdaModel(bow_corpus, id2word=dictionary, num_topics=100)
-print(model)
+    # Count word frequencies.
+    frequency = defaultdict(int)
 
-print(model.print_topics(2))
-model.save("./andre_and_heather_miraculous_model")
+    for text in texts:
+
+        for token in text:
+
+            frequency[token] += 1
+
+    # Only keep words that appear more than once.
+    processed_corpus = [[token for token in text if frequency[token] > 1] for text in texts]
+    pprint.pprint(processed_corpus)
+
+    dictionary = corpora.Dictionary(processed_corpus)
+
+    pprint.pprint(dictionary.token2id)
+
+    new_doc = "Human computer interaction"
+    new_vec = dictionary.doc2bow(new_doc.lower().split())
+
+    bow_corpus = [dictionary.doc2bow(text) for text in processed_corpus]
+
+    model = models.LdaModel(bow_corpus, id2word=dictionary, num_topics=100)
+    print(model)
+
+    print(model.print_topics(2))
+    model.save('./output_data/model')
+
+
+def read_text_file(file_path):
+    '''
+    A function for reading in a single file.
+    '''
+
+    # Open the file and store a pointer in memory.
+    with open(file_path, 'r') as f:
+
+        try:
+            # Read the file.
+            data = f.read()
+
+        # Some files throw decode errors because of characters. Catch these.
+        except UnicodeDecodeError:
+
+            print('There was an error decoding the file. Skipping...')
+
+            # Since there was an error return nothing.
+            return None
+
+    # Return the file.
+    return data
+
+
+def ingest_data():
+    '''
+    A function for ingesting the relevant data before building the model.
+    '''
+
+    print('Creating list object for corpus...')
+
+    # Create an empty list to hold the entire corpus and a counter for the files.
+    text_corpus = []
+    file_count = 0
+    ingest_count = 0
+
+    print('Enumerating over files...')
+
+    # Iterate through all files
+    #for file in os.listdir('./input_data/'):
+    for file in glob.iglob(f'./input_data/*.txt'):
+
+        file_count += 1
+
+        print(f'Ingesting the following file: {file}')
+
+        # Call read text file function to ingest the single file.
+        result = read_text_file(f'{file}')
+
+        # If a file is returned, add to the count and our corpus.
+        if result is not None:
+
+            ingest_count += 1
+
+            print('File read. Appending to corpus list...')
+
+            text_corpus.append(result)
+
+    print(f'Ingested a total of {ingest_count} files out of {file_count} files...')
+
+    return text_corpus
+
+
+def main():
+    '''
+    The main function for orchestrating the python script.
+    '''
+
+    print('Ingesting data...')
+
+    text_corpus = ingest_data()
+
+    train_model(text_corpus)
+
+
+if __name__ == '__main__':
+
+    print('Initiating script...')
+
+    main()
